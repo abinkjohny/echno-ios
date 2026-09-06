743fb9eb68b215a92292381eedaa978132f65bbe
Abin K Johny <abinkjohny2011@gmail.com>
2026-09-06 11:45:58 +0530

    Initial Commit

---
+++ echno-ios/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  echno-ios
+//
+//  Created by Ezio Aldetore on 06/09/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ echno-ios/echno_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  echno_iosApp.swift
+//  echno-ios
+//
+//  Created by Ezio Aldetore on 06/09/26.
+//
+
+import SwiftUI
+
+@main
+struct echno_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
